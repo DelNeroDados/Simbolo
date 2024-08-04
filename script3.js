913b4b20afaa4c6f8864698bac4da5937c17348f
Luiz Carlos T. Del Nero <161067539+DelNeroDados@users.noreply.github.com>
2024-08-04 13:55:32 -0300

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,5 +1,3 @@
-/* ########################################################################################## */
-
 document.getElementById('dataForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
@@ -23,71 +21,67 @@ document.getElementById('dataForm').addEventListener('submit', function(event) {
     });
   });
 
-  /* ########################################################################################## */
+/* ########################################################################################## */
     
-    /*Número do seu pedido no nosso site*/
-    $('#n_pedido').mask('0000');
-    /*Data*/
-    $('#date').mask('00/00/0000');
+$('#n_pedido').mask('0000');
+$('#date').mask('00/00/0000');
+
+function applyPhoneMask(input, isCellular) {
+    var mask = isCellular ? '(00) 00000-0000' : '(00) 0000-0000';
+    $(input).mask(mask, { clearIfNotMatch: true });
+}
+
+function updatePhoneMask() {
+    var emerg1Type = $('input[name="contato1"]:checked').val();
+    var emerg2Type = $('input[name="contato2"]:checked').val();
     
-    function applyPhoneMask(input, isCellular) {
-        var mask = isCellular ? '(00) 00000-0000' : '(00) 0000-0000';
-        $(input).mask(mask, { clearIfNotMatch: true });
-    }
-    
-    function updatePhoneMask() {
-        var emerg1Type = $('input[name="contato1"]:checked').val();
-        var emerg2Type = $('input[name="contato2"]:checked').val();
-        
-        applyPhoneMask('#emerg1', emerg1Type === 'Celular');
-        applyPhoneMask('#emerg2', emerg2Type === 'Celular');
-    }
-    
-    $(document).ready(function () {
-        // Inicializa as máscaras com base na seleção inicial
-        updatePhoneMask();
-    
-        // Atualiza as máscaras quando a seleção de rádio muda
-        $('input[name="contato1"]').on('change', function () {
-            applyPhoneMask('#emerg1', $(this).val() === 'Celular');
-        });
-    
-        $('input[name="contato2"]').on('change', function () {
-            applyPhoneMask('#emerg2', $(this).val() === 'Celular');
-        });
+    applyPhoneMask('#emerg1', emerg1Type === 'Celular');
+    applyPhoneMask('#emerg2', emerg2Type === 'Celular');
+}
+
+$(document).ready(function () {
+    updatePhoneMask();
+
+    $('input[name="contato1"]').on('change', function () {
+        applyPhoneMask('#emerg1', $(this).val() === 'Celular');
     });
-    
-    /*INICIO CID IMPUT*/
-    function createCIDInput(index) {
+
+    $('input[name="contato2"]').on('change', function () {
+        applyPhoneMask('#emerg2', $(this).val() === 'Celular');
+    });
+});
+
+/*INICIO CID IMPUT*/
+function createCIDInput(index) {
     return `
         <div class="mb-3">
         <label class="form-label">CID ${index + 1}</label>
-        <input type="text" class="form-control cid-input" placeholder="CID ${index + 1}" required>
+        <input type="text" class="form-control cid-input" name="cid-${index}" placeholder="CID ${index + 1}" required>
         </div>
     `;
-    }
-    
-    function applyCIDMask(input) {
+}
+
+function applyCIDMask(input) {
     $(input).on('input', function () {
         let value = $(this).val().toUpperCase();
         let maskedValue = value;
-    
+
         if (/^[A-Z][0-9]{0,2}$/.test(value)) {
-        maskedValue = value;
+            maskedValue = value;
         } else if (/^[A-Z][0-9]{2}\.?[0-9]?$/.test(value)) {
-        maskedValue = value.replace(/^([A-Z][0-9]{2})([0-9])$/, '$1.$2');
+            maskedValue = value.replace(/^([A-Z][0-9]{2})([0-9])$/, '$1.$2');
         } else if (/^[0-9][A-Z][0-9]{2}$/.test(value)) {
-        maskedValue = value;
+            maskedValue = value;
         } else if (/^[0-9][A-Z][0-9]{2}\.?[A-Z0-9]?$/.test(value)) {
-        maskedValue = value.replace(/^([0-9][A-Z][0-9]{2})([A-Z0-9])$/, '$1.$2');
+            maskedValue = value.replace(/^([0-9][A-Z][0-9]{2})([A-Z0-9])$/, '$1.$2');
         } else {
-        maskedValue = value.substring(0, 5);
+            maskedValue = value.substring(0, 5);
         }
         $(this).val(maskedValue);
     });
-    }
-    
-    $('#numCIDs').on('change', function () {
+}
+
+$('#numCIDs').on('change', function () {
     const numCIDs = parseInt($(this).val());
     const cidInputsContainer = $('#cidInputs');
     cidInputsContainer.empty();
@@ -98,67 +92,60 @@ document.getElementById('dataForm').addEventListener('submit', function(event) {
     $('.cid-input').each(function () {
         applyCIDMask(this);
     });
-    });
-    
-    /*FIM CID IMPUT*/
-    
-    
-    /*TELA DE CONFIRMAÇÃO*/
-    
-    // Exibe o overlay de carregamento
-    function showLoadingOverlay() {
+});
+
+/*FIM CID IMPUT*/
+
+/*TELA DE CONFIRMAÇÃO*/
+
+function showLoadingOverlay() {
     document.getElementById('loading-overlay').style.display = 'block';
-    }
-    
-    // Oculta o overlay de carregamento
-    function hideLoadingOverlay() {
+}
+
+function hideLoadingOverlay() {
     document.getElementById('loading-overlay').style.display = 'none';
-    }
-    
-    // Exibe o popup de confirmação
-    function showConfirmation(message) {
+}
+
+function showConfirmation(message) {
     document.getElementById('confirmation-popup').style.display = 'block';
     document.getElementById('confirmation-message').textContent = message;
-    }
-    
-    // Fecha o popup de confirmação
-    document.getElementById('confirmation-button').addEventListener('click', function () {
+}
+
+document.getElementById('confirmation-button').addEventListener('click', function () {
     document.getElementById('confirmation-popup').style.display = 'none';
-    
-    });
-        
-    function handleSubmit(event) {
-        event.preventDefault();
-        showLoadingOverlay();
-    
-        var formData = {
-            n_pedido: $('#n_pedido').val(),
-            email: $('#email').val(),
-            numCIDs: $('#numCIDs').val(),
-            cids: $('.cid-input').map(function () { return $(this).val(); }).get(),
-            termo: $('input[name="termo"]:checked').val(),
-            colar: $('input[name="colar"]:checked').val(),
-            cor: $('input[name="cor"]:checked').val(),
-            simbolo: $('input[name="simbolo"]:checked').val(),
-            nome: $('#nome').val(),
-            data: $('#data').val(),
-            emerg1: $('#emerg1').val(),
-            emerg2: $('#emerg2').val()
-        };
-    
-        google.script.run
-            .withSuccessHandler(function (response) {
-                hideLoadingOverlay();
-                showConfirmation('Formulário enviado com sucesso! Por favor, verifique seu e-mail.');
-            })
-            .withFailureHandler(function (error) {
-                hideLoadingOverlay();
-                showConfirmation('Erro ao enviar o formulário: ' + error.message);
-            })
-            .enviarDados(formData);
-    }
-    
-    $(document).ready(function () {
-        $('#myForm').on('submit', handleSubmit);
-    });
-    /*FIM ENVIO DE FORMULÁRIO*/
+});
+
+function handleSubmit(event) {
+    event.preventDefault();
+    showLoadingOverlay();
+
+    var formData = {
+        n_pedido: $('#n_pedido').val(),
+        email: $('#email').val(),
+        numCIDs: $('#numCIDs').val(),
+        cids: $('.cid-input').map(function () { return $(this).val(); }).get(),
+        termo: $('input[name="termo"]:checked').val(),
+        colar: $('input[name="colar"]:checked').val(),
+        cor: $('input[name="cor"]:checked').val(),
+        simbolo: $('input[name="simbolo"]:checked').val(),
+        nome: $('#nome').val(),
+        data: $('#data').val(),
+        emerg1: $('#emerg1').val(),
+        emerg2: $('#emerg2').val()
+    };
+
+    google.script.run
+        .withSuccessHandler(function (response) {
+            hideLoadingOverlay();
+            showConfirmation('Formulário enviado com sucesso! Por favor, verifique seu e-mail.');
+        })
+        .withFailureHandler(function (error) {
+            hideLoadingOverlay();
+            showConfirmation('Erro ao enviar o formulário: ' + error.message);
+        })
+        .enviarDados(formData);
+}
+
+$(document).ready(function () {
+    $('#myForm').on('submit', handleSubmit);
+});
