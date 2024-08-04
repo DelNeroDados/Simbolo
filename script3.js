552718bcaa2f5344ce272ecc4a9ea3cf5596640d
Luiz Carlos T. Del Nero <161067539+DelNeroDados@users.noreply.github.com>
2024-08-04 13:33:24 -0300

    Update script3.js

--- script3.js
+++ script3.js
@@ -24,17 +24,17 @@ document.getElementById('dataForm').addEventListener('submit', function(event) {
   });
 
   /* ########################################################################################## */
-
+    
     /*Número do seu pedido no nosso site*/
     $('#n_pedido').mask('0000');
     /*Data*/
     $('#date').mask('00/00/0000');
-
+    
     function applyPhoneMask(input, isCellular) {
         var mask = isCellular ? '(00) 00000-0000' : '(00) 0000-0000';
         $(input).mask(mask, { clearIfNotMatch: true });
     }
-
+    
     function updatePhoneMask() {
         var emerg1Type = $('input[name="contato1"]:checked').val();
         var emerg2Type = $('input[name="contato2"]:checked').val();
@@ -42,21 +42,21 @@ document.getElementById('dataForm').addEventListener('submit', function(event) {
         applyPhoneMask('#emerg1', emerg1Type === 'Celular');
         applyPhoneMask('#emerg2', emerg2Type === 'Celular');
     }
-
+    
     $(document).ready(function () {
         // Inicializa as máscaras com base na seleção inicial
         updatePhoneMask();
-
+    
         // Atualiza as máscaras quando a seleção de rádio muda
         $('input[name="contato1"]').on('change', function () {
             applyPhoneMask('#emerg1', $(this).val() === 'Celular');
         });
-
+    
         $('input[name="contato2"]').on('change', function () {
             applyPhoneMask('#emerg2', $(this).val() === 'Celular');
         });
     });
-
+    
     /*INICIO CID IMPUT*/
     function createCIDInput(index) {
     return `
@@ -66,12 +66,12 @@ document.getElementById('dataForm').addEventListener('submit', function(event) {
         </div>
     `;
     }
-
+    
     function applyCIDMask(input) {
     $(input).on('input', function () {
         let value = $(this).val().toUpperCase();
         let maskedValue = value;
-
+    
         if (/^[A-Z][0-9]{0,2}$/.test(value)) {
         maskedValue = value;
         } else if (/^[A-Z][0-9]{2}\.?[0-9]?$/.test(value)) {
@@ -86,7 +86,7 @@ document.getElementById('dataForm').addEventListener('submit', function(event) {
         $(this).val(maskedValue);
     });
     }
-
+    
     $('#numCIDs').on('change', function () {
     const numCIDs = parseInt($(this).val());
     const cidInputsContainer = $('#cidInputs');
@@ -99,43 +99,43 @@ document.getElementById('dataForm').addEventListener('submit', function(event) {
         applyCIDMask(this);
     });
     });
-
+    
     /*FIM CID IMPUT*/
-
-
+    
+    
     /*TELA DE CONFIRMAÇÃO*/
-
+    
     // Exibe o overlay de carregamento
     function showLoadingOverlay() {
     document.getElementById('loading-overlay').style.display = 'block';
     }
-
+    
     // Oculta o overlay de carregamento
     function hideLoadingOverlay() {
     document.getElementById('loading-overlay').style.display = 'none';
     }
-
+    
     // Exibe o popup de confirmação
     function showConfirmation(message) {
     document.getElementById('confirmation-popup').style.display = 'block';
     document.getElementById('confirmation-message').textContent = message;
     }
-
+    
     // Fecha o popup de confirmação
     document.getElementById('confirmation-button').addEventListener('click', function () {
     document.getElementById('confirmation-popup').style.display = 'none';
-
+    
     });
         
     function handleSubmit(event) {
         event.preventDefault();
         showLoadingOverlay();
-
+    
         var formData = {
             n_pedido: $('#n_pedido').val(),
             email: $('#email').val(),
             numCIDs: $('#numCIDs').val(),
-            cidInputs: $('.cid-input').map(function () { return $(this).val(); }).get(),
+            cids: $('.cid-input').map(function () { return $(this).val(); }).get(),
             termo: $('input[name="termo"]:checked').val(),
             colar: $('input[name="colar"]:checked').val(),
             cor: $('input[name="cor"]:checked').val(),
@@ -145,7 +145,7 @@ document.getElementById('dataForm').addEventListener('submit', function(event) {
             emerg1: $('#emerg1').val(),
             emerg2: $('#emerg2').val()
         };
-
+    
         google.script.run
             .withSuccessHandler(function (response) {
                 hideLoadingOverlay();
@@ -157,7 +157,7 @@ document.getElementById('dataForm').addEventListener('submit', function(event) {
             })
             .enviarDados(formData);
     }
-
+    
     $(document).ready(function () {
         $('#myForm').on('submit', handleSubmit);
     });
